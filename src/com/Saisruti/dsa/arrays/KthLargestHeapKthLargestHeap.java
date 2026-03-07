e73b0277b27011845d9d85950ac783e5b727ed7f
Saisruti <saisrutimohanty96@gmail.com>
2026-03-07 21:53:19 +0530

    Use a Min Heap to keep track of the k largest elements.
    Steps:
    
    Insert elements into the heap.
    
    If heap size exceeds k, remove the smallest element.
    
    The top of the heap will be the kth largest element.
    
    Time Complexity
    
    O(n log k)
    
    Space Complexity
    
    O(k)

---
+++ src/com/Saisruti/dsa/arrays/KthLargestHeapKthLargestHeap.java
@@ -0,0 +1,28 @@
+package com.Saisruti.dsa.arrays;
+import java.util.PriorityQueue;
+public class KthLargestHeapKthLargestHeap {
+
+        public static int findKthLargest(int[] arr, int k) {
+
+            PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+
+            for (int num : arr) {
+                minHeap.add(num);
+
+                if (minHeap.size() > k) {
+                    minHeap.poll(); // remove smallest
+                }
+            }
+
+            return minHeap.peek(); // kth largest
+        }
+
+        public static void main(String[] args) {
+
+            int arr[] = {7, 10, 4, 3, 20, 15};
+            int k = 3;
+
+            System.out.println("Kth Largest Element: " + findKthLargest(arr, k));
+        }
+    }
+
