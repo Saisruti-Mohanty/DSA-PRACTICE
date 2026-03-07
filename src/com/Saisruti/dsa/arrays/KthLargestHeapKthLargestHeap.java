cab27523f75f93f33b3929c48f95cb02f6521ed7
Saisruti <saisrutimohanty96@gmail.com>
2026-03-07 22:09:17 +0530

    added kth largest heap code

--- src/com/Saisruti/dsa/arrays/KthLargestHeapKthLargestHeap.java
+++ src/com/Saisruti/dsa/arrays/KthLargestHeapKthLargestHeap.java
@@ -2,25 +2,21 @@ package com.Saisruti.dsa.arrays;
 import java.util.PriorityQueue;
 public class KthLargestHeapKthLargestHeap {
 
-        public static int findKthLargest(int[] arr, int k) {
-
-            PriorityQueue<Integer> minHeap = new PriorityQueue<>();
-
-            for (int num : arr) {
-                minHeap.add(num);
-
-                if (minHeap.size() > k) {
-                    minHeap.poll(); // remove smallest
-                }
-            }
-
-            return minHeap.peek(); // kth largest
-        }
-
+       public static int findKthLargest(int [] arr,int k) {
+           PriorityQueue<Integer> minHeap = new PriorityQueue<>(); //CREATES A MIN HEAP
+           //TRAVERSE THE ARRAY
+           for (int num : arr) {
+               minHeap.add(num);
+               if (minHeap.size() > k) {
+                   minHeap.poll();//remove smallest
+               }
+           }
+           return minHeap.peek();//kth largest
+       }
         public static void main(String[] args) {
 
             int arr[] = {7, 10, 4, 3, 20, 15};
-            int k = 4;
+            int k = 3;
 
             System.out.println("Kth Largest Element: " + findKthLargest(arr, k));
         }
