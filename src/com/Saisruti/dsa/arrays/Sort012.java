0bfbdc4d33cafd7013140798099b2d7e089bd29d
Saisruti <saisrutimohanty96@gmail.com>
2026-03-12 21:02:12 +0530

    "feat: add Sort 0-1-2 (Dutch National Flag) solution"

---
+++ src/com/Saisruti/dsa/arrays/Sort012.java
@@ -0,0 +1,37 @@
+package com.Saisruti.dsa.arrays;
+//0->swap with low,move low and mid
+//1->just move mid
+public class Sort012 {
+    public static void sortArray(int arr[]){
+        int low=0;
+        int mid=0;
+        int high=arr.length-1;
+        while(mid<=high){
+            if(arr[mid]==0){
+                int temp=arr[low];
+                arr[low]=arr[mid];
+                arr[mid]=temp;
+
+                low++;
+                mid++;
+            }
+            else if (arr[mid]==1){
+                mid++;
+            }
+            else{ //2->swap mid with high,move high
+                int temp=arr[mid];
+                arr[mid]=arr[high];
+                arr[high]=temp;
+
+                high--;
+            }
+        }
+    }
+    public static void main(String[] args){
+        int arr[]={2,1,1,2,0,1,2};
+        sortArray(arr);
+        for(int i:arr){
+            System.out.print(i+ " ");
+        }
+    }
+}
